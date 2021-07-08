2a7b6dc2d8932530e7634b439176fa5f3e3c3a97
erobbi <elirobbins0@gmail.com>
2021-07-07 22:20:36 -0400

    corrected fetchUser

--- js/index.js
+++ js/index.js
@@ -5,18 +5,18 @@ document.addEventListener('DOMContentLoaded', callFunction)
 
 function callFunction() {
     console.log('hello from callFunction')
-    fetchAllUsers()
     document.getElementById('github-form').addEventListener('submit', (e) => {
         e.preventDefault()
         console.log('hello form form')
         let nameValue = document.getElementById('search').value
         console.log(nameValue)
+        fetchUser(nameValue)
     })
 }
 
 
 function fetchUser(nameValue) {
-    fetch(`https://api.github.com/users${nameValue}`)
+    fetch(`https://api.github.com/${nameValue}`)
     .then(res => res.json())
     .then(console.log)
 }
